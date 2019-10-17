2c2896effe94a870afd72cac38150be18d0c4162
samuelpacheco270 <sp6236@thewcs.org>
2019-10-17 21:57:30 +0000

    added click handler in Javascript and changed font family

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
+$("#button").click(function(){
+   var inputtext = $(".input").val();
+   alert(inputtext);
+}
 
 
-  
-  
-  
-  
+
+
+
 });
